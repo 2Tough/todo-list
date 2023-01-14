6d2abfdfc242157ae5804a91019a4d2a34c2f828
Valentin <vainferro@gmail.com>
2023-01-14 23:10:52 +0300

    Reinstalled  added input for note body.

--- src/default-to-do.js
+++ src/default-to-do.js
@@ -3,13 +3,20 @@ import { blankToDo } from "./blank-to-do";
 export function defaultToDo () {
 
     const contentDiv = document.getElementById("content");
+    
     const heading = document.createElement("h1");
-    heading.textContent = "Your ToDo";
+    heading.textContent = "Your ToDo today";
     contentDiv.appendChild(heading);
 
     const projectsDiv = document.createElement("div");
     projectsDiv.textContent = blankToDo().projectTitle;
     contentDiv.appendChild(projectsDiv);
+
+    const inputTitle = document.createElement("input");
+    inputTitle.classList.add("inputTitle");
+    contentDiv.appendChild(inputTitle);
+
+    
 
     // default styling can go here.
 
