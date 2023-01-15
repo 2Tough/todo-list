9e22269fa93695aef302edc7d1c2caa5a2d46d83
Valentin <vainferro@gmail.com>
2023-01-16 00:27:43 +0300

    Updated new-to-do.

--- src/new-to-do.js
+++ src/new-to-do.js
@@ -1,7 +1,19 @@
 let toDoArray = [];
 
 export const newToDo = (Title, Description, DueDate, Priority, CheckList) => {
-    toDoArray.push({Title, Description, DueDate, Priority, CheckList})
+    const getNewToDo = () => {
+        Title
+        Description
+        DueDate
+        Priority
+        CheckList
+    }
+    
+    //toDoArray.push({Title, Description, DueDate, Priority, CheckList})
 
     return {Title, Description, DueDate, Priority, CheckList};
 }
+
+
+
+// Create constructor or class here
