028bec70707026c7c83ef274fa13d3f114c8ab22
Polaski <vainferro@gmail.com>
2023-05-22 20:11:16 +0300

    Created working folder factory.

--- src/defaultTodo.js
+++ src/defaultTodo.js
@@ -1,19 +1,16 @@
-
-
-
 export function defaultTodo() {
 
     const createFolderFactory = (name) => {
         return {
-            name
+            folder: 
+            name,
+            todo: ""
         }
     }
 
     let defaultFolder = createFolderFactory('Default')
-    let folders = [defaultFolder]
+    let folders = defaultFolder
     let foldersDirectory = []
-    
-    console.log(folders)
     
     const todoFactory = (title, description, priority, dueDate) => {
         return {
@@ -24,41 +21,49 @@ export function defaultTodo() {
         }
     }
     
-    
 
+    
     function createTodo(title, description, priority, dueDate, folder) {
         let todo = todoFactory(title, description, priority, dueDate)
-        if(!folder) {
-            folders.push(todo)
-            foldersDirectory.push(folders)
+        if(folder === "") {
+            folders.todo = todo
+            foldersDirectory.push(defaultFolder)
         } else {
-            foldersDirectory.push(createFolderFactory(folder))
-            foldersDirectory.folder.push(todo)
+            let newFolder = createFolderFactory(folder)
+            newFolder.name = todo
+            foldersDirectory.push(newFolder)
         }
+        return todo
     }
 
-    const note0 = createTodo("Go to church", "Need God", "1", "Sunday", "Mass")
+    const note0 = createTodo("Go to church", "Need God", "1", "today", "Mass")
+    const notetest = createTodo("test", "test", "1", "test", "")
+    
+    
+    console.log(foldersDirectory)
 
-    console.log(note0, folders, foldersDirectory)
+    
+    // console.log(note0)
+    // console.log(notetest)
+    
+
 
 
     const note1 = todoFactory("Buy eggs", "Need to get some protein", "1", "Today")
     const note2 = todoFactory("Train", "I need to train", "1", "")
     const note3 = todoFactory("Program", "Need to program", "1", "")
 
-    console.log(note1, note2, note3)
+    // console.log(note1, note2, note3)
 
-    const newTodo = (() => {
-        const noteOne = todoFactory("Get water", "Need more water", "1", "")
+    // const newTodo = (() => {
+    //     const noteOne = todoFactory("Get water", "Need more water", "1", "")
     
-        console.log(noteOne)
-    }
+    //     console.log(noteOne)
+    // }
         
-    )()
+    // )()
     
     
    return todoFactory
 
 }
-
-
