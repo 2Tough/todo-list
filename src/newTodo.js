251ec14d7ccfdf05cf32c8f0d187dade683c6734
Polaski <vainferro@gmail.com>
2023-06-04 17:33:30 +0300

    Updated sidepanel and it's initial structure.

--- src/newTodo.js
+++ src/newTodo.js
@@ -8,17 +8,41 @@ export function newTodo() {
     contentDiv.appendChild(sidePanelDiv);
 
     const todoDiv = document.createElement('div');
-    todoDiv.classList.add('todoDiv')
+    todoDiv.classList.add('todoDiv');
     sidePanelDiv.appendChild(todoDiv);
 
-    const inputTodoTitle = document.createElement('input')
+    const inputTodoTitleLabel = document.createElement('label');
+    inputTodoTitleLabel.textContent = 'Title';
+    inputTodoTitleLabel.classList.add('labels');
+    const inputTodoTitle = document.createElement('input');
+    
 
-    const inputTodoPriority = document.createElement('input')
+    const inputTodoDescriptionLabel = document.createElement('label');
+    inputTodoDescriptionLabel.textContent = 'Description';
+    inputTodoDescriptionLabel.classList.add('labels');
+    const inputTodoDescription = document.createElement('input');
 
-    const inputTodoDueDate = document.createElement('input')
+    const inputTodoPriorityLabel = document.createElement('label');
+    inputTodoPriorityLabel.textContent = 'Priority';
+    inputTodoPriorityLabel.classList.add('labels');
+    const inputTodoPriority = document.createElement('input');
 
-    const inputTodoDFolder = document.createElement('input')
-    todoDiv.append(inputTodoDFolder, inputTodoDueDate, inputTodoPriority,inputTodoTitle );
+    const inputTodoDateLabel = document.createElement('label');
+    inputTodoDateLabel.textContent = 'Date';
+    inputTodoDateLabel.classList.add('labels');
+    const inputTodoDueDate = document.createElement('input');
+
+    const inputTodoDateFolderLabel = document.createElement('label');
+    inputTodoDateFolderLabel.textContent = 'Folder';
+    inputTodoDateFolderLabel.classList.add('labels');
+    const inputTodoDFolder = document.createElement('input');
+
+    todoDiv.append(
+                   inputTodoTitle, inputTodoTitleLabel,
+                   inputTodoDescription, inputTodoDescriptionLabel, 
+                   inputTodoPriority, inputTodoPriorityLabel, 
+                   inputTodoDueDate, inputTodoDateLabel, 
+                   inputTodoDFolder, inputTodoDateFolderLabel );
     
 
 }
